8c1397b5f6f0fb09d05bb4e954c89d312711ae8d
Parth Ahuja <parthahuja.in@gmail.com>
2024-09-03 00:53:56 +0530

    localhost changed in typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,8 @@ import { ProductSize } from './shop/entities/product-size.entity';
     ShopModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      // host change from localhost to 127.0.0.1
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: 'Htrap@1234',
